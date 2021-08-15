7e7140fa3a65efd080acbab525c0a3b8acbe8923
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-08-15 10:04:03 -0300

    feature:usando o assert

---
+++ funcoes/gerador_html.py
@@ -0,0 +1,14 @@
+from typing import Text
+
+
+def tag_bloco(texto, classe='sucess'):
+    return f'<div class="{classe}">{texto}</div>'
+
+if __name__ == '__main__':
+    #testes com assertions
+    assert tag_bloco('incluindo texto') ==\
+        '<div class="sucess">incluindo texto</div>'
+    assert tag_bloco('excluir conteudo','error') ==\
+        '<div class="error">excluir conteudo</div>'   
+
+    print(tag_bloco('bloco'))     
