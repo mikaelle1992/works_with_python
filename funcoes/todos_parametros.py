6b59cd6f7a167581d5dc84dc7fd9e1fddb21faee
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-08-25 07:42:25 -0300

    adjust:parametros da função

--- funcoes/todos_parametros.py
+++ funcoes/todos_parametros.py
@@ -2,9 +2,10 @@ def todos_params(*args, **kwargs):
     print(f'args: {args}')
     print(f'kwargs: {kwargs}')
 
-if __name__ == '__main__': 
-    todos_params('a','b','c')
-    todos_params(1, 2, 3, sol = False, lua =  True)
-    todos_params('Ana', True, [1, 2, 3], tamano = 'G', altura = 1.67)
-    todos_params(primeiro =  'Pinheiro', segundo = 'Sousa')
-    todos_params('Sousa', primeiro = 'Pinheiro')
+
+if __name__ == '__main__':
+    todos_params('a', 'b', 'c')
+    todos_params(1, 2, 3, sol=False, lua=True)
+    todos_params('Ana', True, [1, 2, 3], tamano='G', altura=1.67)
+    todos_params(primeiro='Pinheiro', segundo='Sousa')
+    todos_params('Sousa', primeiro='Pinheiro')
