223902c2ea89b7f2153241a657c916a26434f070
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2021-10-24 13:57:52 -0300

    função lambda

---
+++ programacao_funcional /funcoes_lambda.py
@@ -0,0 +1,14 @@
+compras = (
+    {'quantidade': 2, 'preco': 10},
+    {'quantidade': 3, 'preco': 50},
+    {'quantidade': 5, 'preco': 30},
+)
+
+totais = tuple(
+    map(
+        lambda compra: compra['quantidade'] * compra['preco'],
+        compras
+    )
+)
+
+print('Preços totais:', list(totais))
