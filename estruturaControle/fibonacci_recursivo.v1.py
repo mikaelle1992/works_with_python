608b48277ba0bac74afd13fc03eabf994ef8817b
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2021-10-24 17:45:48 -0300

    fix: Indentação do código

--- estruturaControle/fibonacci_recursivo.v1.py
+++ estruturaControle/fibonacci_recursivo.v1.py
@@ -1,12 +1,9 @@
-def fibonacci(qtd, sequencia = (0, 1)):
-    
-   if len(sequencia) == qtd:
-       return sequencia
-   return fibonacci(qtd, sequencia + (sum(sequencia[-2:]),))    
-
-
+def fibonacci(qtd, sequencia=(0, 1)):
+    if len(sequencia) == qtd:
+        return sequencia
+    return fibonacci(qtd, sequencia + (sum(sequencia[-2:]),))
 
 
 if __name__ == '__main__':
-        for fib in fibonacci(3):
-            print(fib)
+    for fib in fibonacci(4):
+        print(fib)
