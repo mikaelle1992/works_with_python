0fdb452b161254ca43700e66de4bd46e1dd86e35
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2022-01-19 22:14:30 -0300

    feature: criando tabelas e inserindo dados

---
+++ database/inserir_estados1.sql
@@ -0,0 +1,9 @@
+insert into estados(nome, sigla, regio, populacao)
+values('Acre', 'AC', 'Norte', 0.83);
+
+insert into estados
+    (nome, sigla, regio, populacao)
+values
+    ('Alagoas', 'AL', 'Nordeste', 3.38),
+    ('Amapá', 'AP', 'Norte', 0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
