45c6feb5cee3470dada10ba875b02a0fc0a8f275
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2022-01-24 08:28:58 -0300

    Fix: limite no select

--- banco_de_dados/selecionar_contatos.py
+++ banco_de_dados/selecionar_contatos.py
@@ -1,14 +1,16 @@
 from bd import nova_conexao
 from mysql.connector import ProgrammingError
 
-sql = 'SELECT * FROM contatos'
+sql = 'SELECT * FROM contatos LIMIT %s OFFSET %s'
+# limites do select
+# sql = 'SELECT * FROM contatos'
 
 
 try:        
     with nova_conexao() as conexao:
         try:
             cursor = conexao.cursor()
-            cursor.execute(sql)   
+            cursor.execute(sql, (2, 2))
             contatos = cursor.fetchall()
            
         except ProgrammingError as e :
